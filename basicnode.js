40623e39f19e13f30f544a98c5937fd9aed3dde7
Balamurugan Madhaiyan <BalamuruganM@pricoltech.com>
2017-08-10 18:49:14 +0530

    Appar perumaan thiruvadi potri potri

--- basicnode.js
+++ basicnode.js
@@ -1,6 +1,6 @@
-var http = require('http');
+var http = require('http');              
 
-http.createServer(function (req, res) {
+http.createServer(function (req, res) {    
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Appar Perumaan thiruvadi potri potri');
-}).listen(8080);
+    res.end('Appar Perumaan thiruvadi potri potri');                  
+}).listen(8080);              
