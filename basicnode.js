4c1030ecb938e77f3a51aaadbef19b03c6678a1c
Balamurugan <balamurugan.madhaiyan@pricoltech.com>
2017-08-10 18:39:06 +0530

    basic file added

---
+++ basicnode.js
@@ -0,0 +1,6 @@
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Appar Perumaan thiruvadi potri potri');
+}).listen(8080);
